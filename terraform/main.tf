287c506f28572c5f7fd11fdb035725b72dbc175f
Brian Sayler <saylerb@users.noreply.github.com>
2026-04-16 11:05:55 -0600

    must disable block and restrict public access to add a public access policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,9 +29,9 @@ resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "site" {
